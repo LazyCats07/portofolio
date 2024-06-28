4b90bd15b8fd34ac0845b1652999ae2edcce4feb
edianantarafi <edianantarafi@gmail.com>
2024-06-28 14:23:15 +0700

    edit sumfetch

--- src/utils/bin/sumfetch.ts
+++ src/utils/bin/sumfetch.ts
@@ -2,23 +2,53 @@ import config from '../../../config.json';
 
 const sumfetch = async (args: string[]): Promise<string> => {
   return `
-           ▄▓▓▓▓▓▓▓▓▓▓▓▓▓▓▄                  Summary
-        ▄▓▓▀ ▄▓▓▀▓▓▓▀▓▓▄ ▀▀▓▓▄              -----------
-      ▓▓▀  ▄▓▀   ▓▓▓  ▀▓▓    ▓▓▄             ABOUT
-    ▄▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓            ${config.name}
-   ▓▓     ▓▓▓    ▓▓▓    ▐▓▓     ▓▓           <u><a href="${config.resume_url}" target="_blank">resume</a></u>
-▐▓▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▓       爵 <u><a href="${config.repo}" target="_blank">Github repo</a></u>
-▐▓                                 ▐▓       -----------
-▐▓           > Portfolio           ▐▓        CONTACT
-▐▓                                 ▐▓        <u><a href="mailto:${config.email}" target="_blank">${config.email}</a></u>
-▐▓▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▓        <u><a href="https://github.com/${config.social.github}" target="_blank">github.com/${config.social.github}</a></u>
-   ▓▓      ▐▓▓    ▓▓    ▐▓▓     ▓▓           <u><a href="https://linkedin.com/in/${config.social.linkedin}" target="_blank">linkedin.com/in/${config.social.linkedin}</a></u>
-    ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓           -----------
-      ▓▓▓   ▐▓▓   ▓▓   ▓▓▓   ▓▓▀            
-         ▀▓▓▄▄▓▓▓▄▓▓▄▓▓▓▄▄▓▓▀               
-            ▓▓▓▓▓▓▓▓▓▓▓▓▓▓
-
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@       Summary
+@@@@@&&&&&&&&&&&&//////%%%%%%%@@@@@@@@@      -----------
+@@@@@&&&&&&&&&&&#//////&&&&&&&@@@@@@@@@       ABOUT
+@@@@@/////////////////%%%%%%%%@@@@@@@@@       ${config.name}
+@@@@@&&&&&&&&&&&////#%@@@@@@@@@@@@@@@@@       <u><a href="${config.resume_url}" target="_blank">Resume</a></u>
+@@@@@&&&&&&&&&//////%%%%%%%%%%@@@@@@@@@      爵 <u><a href="${config.repo}" target="_blank">Github Repo</a></u>
+@@@@@%%%%%%%%(///////%%%%%%%%%@@@@@@@@@      爵 <u><a href="${config.Medium}" target="_blank">Medium</a></u>
+@@@@@&&&&&&&&/////////&&&&&&&&@@@@@@@@@      -----------      
+@@@@@%%%%%%///////////########@@@@@@@@@       CONTACT
+@@@@@@@@@@@///////////&&&&&%@@@@@@@@@@@      爵 <u><a href="${config.email}" target="_blank">Linkedin</a></u>
+@@@@@@@@@@//////////////@%&%@@@@@@@@@@@      爵 <u><a href="${config.linkedin}" target="_blank">Linkedin</a></u>
+@@@@@%%%%%//#/////////&#////@@@@@@@@@@@      爵 <u><a href="${config.whatsapp_url}" target="_blank">Whatsapp</a></u>
+@@@@@&&&&///@/////////##&&///@@@@@@@@@@      爵 <u><a href="${config.Instagram}" target="_blank">Instagram</a></u>
+@@@@@&&&///%///////////&&&(%%@@@@@@@@@@      -----------
+@@@@@&&&&/##///////////&&&&%%@@@@@@@@@@
+@@@@@&&&&((/////////////%%%%%%@@@@@@@@@
+@@@@@&&&&&&///////@//////%%%%%@@@@@@@@@
+@@@@@&&&&&&//////&&&//////&&&&@@@@@@@@@
+@@@@@&&&&&/////@@@&&&&/////&&&@@@@@@@@@
+@@@@@&&&(////%&&&&&&&&&/////@@@@@@@@@@@
+@@@@@@@/////@@@@@@%%%%%%/////#@@@@@@@@@
+@@@@@(////&&&&&&&&&&&&&&&/////%%%%#%@@@
+@@@@@////@@@@@@@@@%%%%%%%%%/////(///@@@
+@@@@@@////&&&&&&&&&@@@@@@@@@&&&&&&%%@@@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@                
+                                                               
 `;
 };
 
 export default sumfetch;
+
+
+//            ▄▓▓▓▓▓▓▓▓▓▓▓▓▓▓▄                  Summary
+//         ▄▓▓▀ ▄▓▓▀▓▓▓▀▓▓▄ ▀▀▓▓▄              -----------
+//       ▓▓▀  ▄▓▀   ▓▓▓  ▀▓▓    ▓▓▄             ABOUT
+//     ▄▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓            ${config.name}
+//    ▓▓     ▓▓▓    ▓▓▓    ▐▓▓     ▓▓           <u><a href="${config.resume_url}" target="_blank">resume</a></u>
+// ▐▓▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▓       爵 <u><a href="${config.repo}" target="_blank">Github repo</a></u>
+// ▐▓                                 ▐▓       -----------
+// ▐▓           > Portfolio           ▐▓        CONTACT
+// ▐▓                                 ▐▓        <u><a href="mailto:${config.email}" target="_blank">${config.email}</a></u>
+// ▐▓▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▓        <u><a href="https://github.com/${config.social.github}" target="_blank">github.com/${config.social.github}</a></u>
+//    ▓▓      ▐▓▓    ▓▓    ▐▓▓     ▓▓           <u><a href="https://linkedin.com/in/${config.social.linkedin}" target="_blank">linkedin.com/in/${config.social.linkedin}</a></u>
+//     ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓           -----------
+//       ▓▓▓   ▐▓▓   ▓▓   ▓▓▓   ▓▓▀            
+//          ▀▓▓▄▄▓▓▓▄▓▓▄▓▓▓▄▄▓▓▀               
+//             ▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+
+    
